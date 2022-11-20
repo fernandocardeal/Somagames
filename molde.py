a86c71512aaf213349b555a7a38c9fecbb7df8b1
Luis Fernando Santos Cardeal <fernandocardeal529@gmail.com>
2022-11-20 09:15:55 -0300

    Atualização

---
+++ molde.py
@@ -0,0 +1,31 @@
+molde_placar = '''
+PLACAR FINAL
+
+Game: {}
+Aplicador(a): {}
+Data: {}
+
+GRYFF
+Chamada: {}
+Game: {}
+Descontos: {}
+Total: {}
+
+HUFF
+Chamada: {}
+Game: {}
+Descontos: {}
+Total: {}
+
+RAVEN
+Chamada: {}
+Game: {}
+Descontos: {}
+Total: {}
+
+SLY
+Chamada: {}
+Game: {}
+Descontos: {}
+Total: {}
+'''
